580426b83a9ebbda499e026ade3ffbd8f1d9d508
dodinoveli <dodinovely.petanicoding@gmail.com>
2020-10-23 23:57:40 +0700

    update mlam

--- application/controllers/api/Master.php
+++ application/controllers/api/Master.php
@@ -18,18 +18,63 @@ class Master extends CI_Controller{
 		$res = null;
 		if ($data) {
 			$res = array(
-				"status" =>"true",
+				"result" =>"true",
 				"pesan"  =>$data);
 			echo json_encode($res);
 		}else{
 		   $res = array(
-		   	    "status" =>"true",
+		   	    "result" =>"true",
 				"pesan"  =>$data);
 		   echo json_encode($data);
 		}
 		
 	}
 
-	/*tes aja*/
+	public function subKateGori()
+	{
+		$id   = $this->input->get('kat_id');
+		$sql  = "SELECT
+				master_katrgory.kat_id,
+				master_katrgory.kategori,
+				master_katrgory.image,
+				master_katrgory.tgl,
+				m_kategory.kata_id,
+				m_kategory.judul,
+				m_kategory.isi 
+				FROM master_katrgory INNER JOIN m_kategory ON master_katrgory.kat_id=m_kategory.kat_id 
+				WHERE master_katrgory.kat_id='".$id."'";
+		$data = $this->db->query($sql)->result();
+		$res = null;
+		if ($data) {
+			$res = array(
+				"result" =>"true",
+				"pesan"  =>$data);
+			echo json_encode($res);
+		}else{
+		   $res = array(
+		   	    "result" =>"true",
+				"pesan"  =>$data);
+		   echo json_encode($data);
+		}
+	}
+
+	public function detail()
+	{
+		$id   = $this->input->get('kata_id');
+		$sql  = "SELECT * FROM `m_kategory` WHERE kata_id='".$id."'";
+		$data = $this->db->query($sql)->result();
+		$res = null;
+		if ($data) {
+			$res = array(
+				"result" =>"true",
+				"pesan"  =>$data);
+			echo json_encode($res);
+		}else{
+		   $res = array(
+		   	    "result" =>"true",
+				"pesan"  =>$data);
+		   echo json_encode($data);
+		}
+	}
 }
 ?>
